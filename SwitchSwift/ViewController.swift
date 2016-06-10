7bfe86015563e13998c848701ddaaaf950364e03
Sandeep Bhandari <sandeepbhandari@Sandeeps-iMac.local>
2016-06-10 19:44:33 +0530

    index path selection persistant

--- SwitchSwift/ViewController.swift
+++ SwitchSwift/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController,CellProtocol,UITableViewDelegate,UITableViewDataSource {
     @IBOutlet var tableView: UITableView!
 
-    let data = ["sandeep","Gopal","Bhandari"]
+    let data = ["sandeep","Gopal","Bhandari","sandeep","Gopal","Bhandari","sandeep","Gopal","Bhandari","sandeep","Gopal","Bhandari","sandeep","Gopal","Bhandari","sandeep","Gopal","Bhandari","sandeep","Gopal","Bhandari","sandeep","Gopal","Bhandari","sandeep","Gopal","Bhandari"]
+    var selectedIndexPathArray = [NSIndexPath]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +27,17 @@ class ViewController: UIViewController,CellProtocol,UITableViewDelegate,UITableV
     func switchButtonTapped(WithStatus status: Bool, ForCell cell: myCell) {
         let indexPath = self.tableView .indexPathForCell(cell)
         print("cell at indexpath \(indexPath) tapped with switch status \(status)")
+        
+        if status == true {
+            if self.selectedIndexPathArray.indexOf(indexPath!) == nil {
+                self.selectedIndexPathArray.append(indexPath!)
+            }
+        }
+        else{
+            if let index =  self.selectedIndexPathArray.indexOf(indexPath!) {
+                self.selectedIndexPathArray.removeAtIndex(index)
+            }
+        }
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -35,10 +47,26 @@ class ViewController: UIViewController,CellProtocol,UITableViewDelegate,UITableV
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let tableCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as! myCell
         tableCell.delegate = self
+        tableCell.myLabel.text? = data[indexPath.row]
+        
+        if self.selectedIndexPathArray.contains(indexPath) {
+            tableCell.mySwitch.setOn(true, animated: false)
+        }
+        else{
+            tableCell.mySwitch.setOn(false, animated: false)
+        }
         return tableCell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if let index =  self.selectedIndexPathArray.indexOf(indexPath) {
+            self.selectedIndexPathArray.removeAtIndex(index)
+        }
+        else{
+            self.selectedIndexPathArray.append(indexPath)
+        }
+        
         let cell = self.tableView.cellForRowAtIndexPath(indexPath) as! myCell
         cell.toggleSwitch()
     }
